c2eeccae123e2ba0f627e5479fd1975bc6795a94
Alceu Matteucci Junior <alceumatteuccijunior@gmail.com>
2026-08-11 17:05:30 -0300

    Teste de index

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
-/**
- * teeeeeeeeeeeeeeeeeeeeeeeessssste
- * Orbi Educacional - Root Entry Point
- * Este arquivo fica na raiz (public_html) e faz o roteamento inteligente.
- */
-session_start();
-
-// Se o usuário não estiver logado, redireciona para a tela de Login
-if (!isset($_SESSION['user_id'])) {
-    header("Location: public/login.php");
-    exit;
-}
-
-// Se estiver logado, redireciona conforme o nível de acesso
-if (isset($_SESSION['user_nivel'])) {
-    if ($_SESSION['user_nivel'] === 'admin_master' || $_SESSION['user_nivel'] === 'professor') {
-        header("Location: admin/index.php");
-    } else {
-        header("Location: public/index.php"); // Dashboard do Aluno
-    }
-    exit;
-}
+echo "teste";
 ?>
